de4a9f340568f6f67e9146e133aae8ab1a8c8a93
hatembkh <boukhroufhatem04@gmail.com>
2025-08-12 07:50:42 +0100

    fixing path

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 
-app.use('/uploads', express.static(path.join(__dirname, '../../frontend/public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.json())
 
